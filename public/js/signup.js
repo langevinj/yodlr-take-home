768066e146b2a497bc1f084efe79243eaba06032
Jeremy Langevin <langevinj832@gmail.com>
2021-02-08 09:12:04 -0500

    Handling basic email submit and signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,10 @@
-
-
 const emailForm = document.getElementById('emailForm');
+const BASE_URL = 'http://localhost:3000/users'
 
 emailForm.addEventListener('submit', async function(evt) {
     evt.preventDefault();
     const emailInput = emailForm.elements[0].value;
-    console.log(emailInput);
+    const params = { email: emailInput };
+    const res = await axios.post(BASE_URL, params)
+    console.log(res);
 });
