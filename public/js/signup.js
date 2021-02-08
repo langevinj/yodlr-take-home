c909d20d3748c0e650cfc00b5b9a5b56ebd0704c
Jeremy Langevin <langevinj832@gmail.com>
2021-02-08 09:18:15 -0500

    Addition of firstName and lastName to form

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,10 @@ const BASE_URL = 'http://localhost:3000/users'
 emailForm.addEventListener('submit', async function(evt) {
     evt.preventDefault();
     const emailInput = emailForm.elements[0].value;
-    const params = { email: emailInput };
+    const firstName = emailForm.elements[1].value || "";
+    const lastName = emailForm.elements[2].value || "";
+
+    const params = { email: emailInput, firstName: firstName, lastName: lastName };
     const res = await axios.post(BASE_URL, params)
     console.log(res);
 });
