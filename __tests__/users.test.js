8a867af7078655ac0f84db0ff82153629291554f
Jeremy Langevin <langevinj832@gmail.com>
2021-02-08 12:04:52 -0500

    Tests written for GET /users/:id

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -9,16 +9,15 @@ const request = supertest(app);
 
 
 describe('GET /users', function() {
-    it('responds with a list of users', async function(done) {
+    it('responds with a list of users', async function() {
         const res = await request.get('/users');
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).toEqual(5);
-        done();
     });
 });
 
 describe('POST /users', function() {
-    it('adds a new user', async function(done) {
+    it('adds a new user', async function() {
         const newUserData = {email: "testuser@test.com", firstName: "Test", lastName: "User"}
         const res = await request.post('/users')
                     .send(newUserData)
@@ -30,7 +29,22 @@ describe('POST /users', function() {
             lastName: "User",
             state: expect.any(String)
         });
-        done();
+    });
+});
+
+describe('GET /users/:id', function() {
+    it('gets a user with given id', async function() {
+        const res = await request.get('/users/1');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({ "id": 1, "email": "kyle@getyodlr.com", "firstName": "Kyle", "lastName": "White", "state": "active" });
+    });
+
+    it('returns 404 for non-existent user', async function() {
+        try {
+            const res = await request.get('/users/100000');
+        } catch (err) {
+            expect(err.statusCode).toEqual(404);
+        }
     });
 });
 
