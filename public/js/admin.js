ebb431c824adc85ed0f034556574dc73124463e9
Jeremy Langevin <langevinj832@gmail.com>
2021-02-08 09:26:55 -0500

    Fetching of user's on load

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,11 @@
 
-document.addEventListener('load', async function(){
+const BASE_URL = 'http://localhost:3000/users';
 
+function listUsers(users) {
+
+}
+
+window.addEventListener('load', async function(){
+    const users = await axios.get(BASE_URL);
+    console.log(users);
 });
