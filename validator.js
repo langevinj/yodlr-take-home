d8115b10526f1d3b3841ee15697c75e567d0982c
Jeremy Langevin <langevinj832@gmail.com>
2021-02-09 09:46:28 -0500

    Creation of validation functions

--- validator.js
+++ validator.js
@@ -1,6 +1,40 @@
+const axios = require("axios");
+const acceptalNameCharacters = "abcdefghijklmnopqrstuvwxyz- ".split("");
 
-function validateUserSignup(signupData){
+//Make sure there is not already a user with the email provided.
+function checkIfDuplicateEmail(email){
+    const res = await axios.get('http://localhost:3000/users');
+    const existingUsers = res.data;
 
+    for(let user of existingUsers){
+        if(user.email === email) return true
+    }
+
+    return false;
 }
 
-module.exports = validateUserSignup;
+//Make sure that a name on contains valid characters.
+function checkIfNameInvalid(name){
+    for(let char of name.toLowercase()){
+        if(!acceptalNameCharacters.includes(char)) return true;
+    }
+
+    return false;
+}
+
+//Go through each piece of data passed in signup. Determine validity, then create an error array.
+function validateUserSignup(signupData) {
+    const errors = [];
+    const emailInvalid = checkIfDuplicateEmail(signupData.email);
+    const firstNameInvalid = checkIfNameInvalid(signupData.firstName);
+    const lastNameInvalid = checkIfNameInvalid(signupData.lastName);
+
+    if(emailInvalid) errors.push("An account for this email already exists.");
+    if(firstNameInvalid) errors.push("The first name you entered contains invalid characters.");
+    if(lastNameInvalid) errors.push("The last name you entered contains invalid characters.");
+
+    return errors
+}
+
+
+module.exports = { validateUserSignup, checkIfDuplicateEmail, checkIfNameInvalid };
