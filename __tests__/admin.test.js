10e440d5817d4ec70b9357fd1e5acd7e3061b5c2
Jeremy Langevin <langevinj832@gmail.com>
2021-02-09 13:35:08 -0500

    Added connection ender to admin test

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -4,6 +4,11 @@
  */
 
 const createUserRow = require('../public/js/admin.js');
+const {endConnection} = require('../index.js');
+
+afterAll(() => {
+    endConnection();
+});
 
 describe("createUserRow", function() {
     it("creates an HTML table row element containing user data", function() {
@@ -12,3 +17,4 @@ describe("createUserRow", function() {
         expect(testTableRow.outerHTML).toEqual(`<tr id="1"><th scope="row">${userData.id}</th><td>${userData.firstName}</td><td>${userData.lastName}</td><td>${userData.email}</td><td><button class="btn btn-success rounded state-button">${userData.state}</button></td></tr>`);
     });
 });
+
