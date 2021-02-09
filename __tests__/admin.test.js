4bdcce17d576a6998b4e88f08673d33bc05811c5
Jeremy Langevin <langevinj832@gmail.com>
2021-02-09 09:08:47 -0500

    Updated admin page test to reflect addition of state column

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -9,6 +9,6 @@ describe("createUserRow", function() {
     it("creates an HTML table row element containing user data", function() {
         const userData = { "id": 1, "email": "kyle@getyodlr.com", "firstName": "Kyle", "lastName": "White", "state": "active" };
         const testTableRow = createUserRow(userData);
-        expect(testTableRow.outerHTML).toEqual(`<tr><th scope="row">${userData.id}</th><td>${userData.firstName}</td><td>${userData.lastName}</td><td>${userData.email}</td></tr>`)
+        expect(testTableRow.outerHTML).toEqual(`<tr id="1"><th scope="row">${userData.id}</th><td>${userData.firstName}</td><td>${userData.lastName}</td><td>${userData.email}</td><td><button class="btn btn-success rounded state-button">${userData.state}</button></td></tr>`);
     });
 });
